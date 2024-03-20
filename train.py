d6e1d09937970fe37b466deca216d18553c321ee
ppajot <ppajot@student.42.fr>
2024-03-20 03:20:12 +0100

    momentum adam

--- train.py
+++ train.py
@@ -7,6 +7,56 @@ import math
 import matplotlib.pyplot as plt
 from sklearn.neural_network import MLPClassifier
 
+fig, axs = plt.subplots(1, 2)
+
+def get_part_deriv(mlp, delta, batch_index, i):
+	if (i == mlp.size - 1):
+		delta = mlp.layers[i].neurons - mlp.output[batch_index]
+	else:
+		delta = mlp.layers[i].deriv_acti(mlp.layers[i].neurons) * np.dot(delta, mlp.weights[i].T)
+	return delta, np.mean(delta, 0), np.dot(mlp.layers[i - 1].neurons.T, delta) / len(batch_index)
+
+def adam(mlp, batch_index, steps):
+	delta = 0
+	for i in reversed(range(1, mlp.size)):
+		delta, d_bias, d_weights = get_part_deriv(mlp, delta, batch_index, i)
+		mlp.mt_b[i - 1] = mlp.beta1 * mlp.mt_b[i - 1] + (1 - mlp.beta1) * d_bias
+		mlp.mt_w[i - 1] = mlp.beta1 * mlp.mt_w[i - 1] + (1 - mlp.beta1) * d_weights
+
+		mlp.vt_b[i - 1] = mlp.beta2 * mlp.vt_b[i - 1] + (1 - mlp.beta2) * np.power(d_bias, 2)
+		mlp.vt_w[i - 1] = mlp.beta2 * mlp.vt_w[i - 1] + (1 - mlp.beta2) * np.power(d_weights, 2)
+
+		mt_corr_b = np.divide(mlp.mt_b[i - 1], 1 - np.power(mlp.beta1, steps))
+		mt_corr_w = np.divide(mlp.mt_w[i - 1], 1 - np.power(mlp.beta1, steps))
+
+		vt_corr_b = np.divide(mlp.vt_b[i - 1], 1 - np.power(mlp.beta2, steps))
+		vt_corr_w = np.divide(mlp.vt_w[i - 1], 1 - np.power(mlp.beta2, steps))
+
+		mt_corr_b = np.divide(mt_corr_b, np.sqrt(vt_corr_b) + mlp.epsilon)
+		mt_corr_w = np.divide(mt_corr_w, np.sqrt(vt_corr_w) + mlp.epsilon)
+		mlp.bias[i - 1] -= mlp.learning_rate * mt_corr_b
+		mlp.weights[i - 1] -= mlp.learning_rate * mt_corr_w
+
+def gradient_descent(mlp, batch_index, steps):
+	delta = 0
+	for i in reversed(range(1, mlp.size)):
+		delta, d_bias, d_weights = get_part_deriv(mlp, delta, batch_index, i)
+
+		mlp.weights[i - 1] -= mlp.weights[i - 1] * mlp.regul
+
+		mlp.velocity_w[i - 1] = mlp.momentum * mlp.velocity_w[i - 1] - mlp.learning_rate * d_weights
+		mlp.velocity_b[i - 1] = mlp.momentum * mlp.velocity_b[i - 1] - mlp.learning_rate * d_bias
+		
+		mlp.weights[i - 1] += mlp.velocity_w[i - 1]
+		mlp.bias[i - 1] += mlp.velocity_b[i - 1]
+		# mlp.bias[i - 1] -= mlp.bias[i - 1] * mlp.regul / len(batch_index)
+
+
+optimizers = {
+	'sgd': gradient_descent,
+	'adam': adam
+}
+
 class LossAcc:
 	def	__init__(self):
 		self.train_loss = []
@@ -21,7 +71,10 @@ class LossAcc:
 		for i in range (mlp.train_output.size):
 			loss -= math.log(mlp.layers[mlp.size - 1].neurons[i][mlp.train_output[i]])
 			acc += (mlp.train_output[i] == np.argmax(mlp.layers[mlp.size - 1].neurons[i]))
-		self.train_loss.append(loss / mlp.train_output.size)
+		loss /= mlp.train_output.size
+		# for wei in mlp.weights:
+		# 	loss += np.sum(np.power(wei, 2)) * mlp.regul * mlp.learning_rate
+		self.train_loss.append(loss)
 		self.train_acc.append(acc / mlp.train_output.size)
 		loss = 0
 		acc = 0
@@ -29,15 +82,19 @@ class LossAcc:
 		for i in range (mlp.test_output.size):
 			loss -= math.log(mlp.layers[mlp.size - 1].neurons[i][mlp.test_output[i]])
 			acc += (mlp.test_output[i] == np.argmax(mlp.layers[mlp.size - 1].neurons[i]))
-		self.test_loss.append(loss / mlp.test_output.size)
+		loss /= mlp.test_output.size
+		# for wei in mlp.weights:
+		# 	loss += np.sum(np.power(wei, 2)) * mlp.regul * mlp.learning_rate
+		self.test_loss.append(loss)
 		self.test_acc.append(acc / mlp.test_output.size)
 
 	def show_curves(self):
-		fig, axs = plt.subplots(1, 2)
-		axs[0].plot(range(len(self.train_loss)), self.train_loss)
-		axs[0].plot(range(len(self.test_loss)), self.test_loss)
+		# fig, axs = plt.subplots(1, 2)
+		axs[0].plot(range(len(self.train_loss)), self.train_loss, label='train')
+		axs[0].plot(range(len(self.test_loss)), self.test_loss, label='validation')
 		axs[1].plot(range(len(self.train_acc)), self.train_acc)
 		axs[1].plot(range(len(self.test_acc)), self.test_acc)
+		axs[0].legend()
 		fig.set_size_inches((12, 5))
 		plt.show()
 
@@ -73,22 +130,66 @@ class Layer:
 		'sigmoid': sigmoid,
 		'softmax': softmax,
 		'tanh': (lambda x: np.tanh(x)),
-		'Relu': (lambda x: x*(x > 0)),
+		'relu': (lambda x: x * (x > 0)),
 	}
 
 	deriv_acti = {
 		'sigmoid': sigmoid_der,
 		'softmax': (lambda x: 0),
-		'tanh': (lambda x: 1-x**2),
-		'Relu': (lambda x: 1 * (x>0))
+		'tanh': (lambda x: 1 - x**2),
+		'relu': (lambda x: (x > 0))
 	}
 
 	def init_weights(self, prev_layer_size):
 		self.weights = np.zeros((prev_layer_size, self.size), float)
 		
 
+def normal(rng, F_in, F_out):
+	limit = 0.4
+	return rng.normal(0.0, limit, (F_in, F_out))
+
+def uniform(rng, F_in, F_out):
+	limit = 0.2
+	return rng.uniform(-limit, limit, (F_in, F_out))
+
+def	LCnormal(rng, F_in, F_out):
+	limit = np.sqrt(1 / F_in)
+	return rng.normal(0.0, limit, (F_in, F_out))
+
+def LCuniform(rng, F_in, F_out):
+	limit = np.sqrt(3 / F_in)
+	return rng.uniform(-limit, limit, (F_in, F_out))
+
+def	XGnormal(rng, F_in, F_out):
+	limit = np.sqrt(2 / (F_in + F_out))
+	return rng.normal(0.0, limit, (F_in, F_out))
+
+def XGuniform(rng, F_in, F_out):
+	limit = np.sqrt(6 / (F_in + F_out))
+	return rng.uniform(-limit, limit, (F_in, F_out))
+
+def	Henormal(rng, F_in, F_out):
+	limit = np.sqrt(2 / F_in)
+	return rng.normal(0.0, limit, (F_in, F_out))
+
+def Heuniform(rng, F_in, F_out):
+	limit = np.sqrt(6 / F_in)
+	return rng.uniform(-limit, limit, (F_in, F_out))
+
+distribution = {
+		'normal': normal,
+		'uniform': uniform,
+		'LCnormal': LCnormal,
+		'LCuniform': LCuniform,
+		'XGnormal': XGnormal,
+		'XGuniform': XGuniform,
+		'Henormal': Henormal,
+		'Heuniform': Heuniform,
+
+}
+
 class MultiLayerPerceptron:
-	def __init__(self, epochs = 200, learning_rate = 0.001, batch_size = 1, seed = None):
+	def __init__(self, epochs = 300, learning_rate = 0.01, batch_size = 1, optimizer = 'sgd', regul = 0.9, seed = None, distrib = 'LCuniform', momentum = 0.9):
 		self.epochs = epochs
 		self.learning_rate = learning_rate
 		self.batch_size = batch_size
@@ -97,8 +198,21 @@ class MultiLayerPerceptron:
 		self.layers = []
 		self.weights = []
 		self.bias = []
+		self.optimizer = optimizers[optimizer]
+		self.mt_b = []
+		self.mt_w = []
+		self.vt_b = []
+		self.vt_w = []
+		self.velocity_w = []
+		self.velocity_b = []
+		self.beta1 = 0.9
+		self.beta2 = 0.999
+		self.regul = regul
+		self.epsilon = 1e-8
 		self.rng = np.random.default_rng(seed)
-		
+		self.distrib = distribution[distrib]
+		self.momentum = momentum
+
 	def	__init_data(self, train_input, train_output, test_input, test_output):
 		self.input = np.array(train_input)
 		size_input = train_input.shape[1]
@@ -118,10 +232,19 @@ class MultiLayerPerceptron:
 
 	def	__init_weights(self):
 		for i in range (1, len(self.layers_sizes)):
-			weights = np.array(self.rng.uniform(-1, 1, (self.layers_sizes[i - 1], self.layers_sizes[i])))
+			if self.optimizer == adam:
+				self.mt_b.append(np.zeros((1, self.layers_sizes[i])))
+				self.vt_b.append(np.zeros((1, self.layers_sizes[i])))
+				self.mt_w.append(np.zeros((self.layers_sizes[i - 1], self.layers_sizes[i])))
+				self.vt_w.append(np.zeros((self.layers_sizes[i - 1], self.layers_sizes[i])))
+			lim = np.sqrt(6 / (self.layers_sizes[i - 1] + self.layers_sizes[i]))
+			weights = np.array(self.rng.uniform(-lim, lim, (self.layers_sizes[i - 1], self.layers_sizes[i])))
 			self.weights.append(weights)
-			bias = np.array(self.rng.uniform(-1, 1, (1, self.layers_sizes[i])))
+			bias = np.array(self.rng.uniform(-lim, lim, (1, self.layers_sizes[i])))
 			self.bias.append(bias)
+			if self.momentum != 0:
+				self.velocity_w.append(np.zeros(weights.shape))
+				self.velocity_b.append(np.zeros(bias.shape))
 
 	def	add_layer(self, size, function):
 		self.layers.append(Layer(size, function))
@@ -135,13 +258,15 @@ class MultiLayerPerceptron:
 
 	def	__train(self):
 		curves = LossAcc()
+		steps = 0
 		for i in range (self.epochs):
 			batch_set = batch_init(self.input.shape[0])
 			while (batch_set):
 				batch_index = get_batch(batch_set, self.batch_size)
 				input = self.input[batch_index]
 				self.feedforward(input)
-				self.__backprop(batch_index)
+				steps += 1
+				self.__backprop(batch_index, steps)
 			curves.get_loss_and_acc(self)
 		curves.show_curves()
 
@@ -152,14 +277,15 @@ class MultiLayerPerceptron:
 			self.layers[i].neurons = self.layers[i].activation(self.layers[i].neurons_base)
 		return self.layers[self.size - 1].neurons
 
-	def	__backprop(self, batch_index):
-		for i in reversed(range(1, self.size)):
-			if (i == self.size - 1):
-				delta = self.layers[i].neurons - self.output[batch_index]
-			else:
-				delta = self.layers[i].deriv_acti(self.layers[i].neurons) * np.dot(delta, self.weights[i].T)
-			self.bias[i - 1] -= self.learning_rate / len(batch_index) * np.sum(delta, 0)
-			self.weights[i - 1] -= self.learning_rate / len(batch_index) * np.dot(self.layers[i - 1].neurons.T, delta)
+	def	__backprop(self, batch_index, steps):
+		self.optimizer(self, batch_index, steps)
+		# for i in reversed(range(1, self.size)):
+		# 	if (i == self.size - 1):
+		# 		delta = self.layers[i].neurons - self.output[batch_index]
+		# 	else:
+		# 		delta = self.layers[i].deriv_acti(self.layers[i].neurons) * np.dot(delta, self.weights[i].T)
+		# 	self.bias[i - 1] -= self.learning_rate / len(batch_index) * np.sum(delta, 0)
+		# 	self.weights[i - 1] -= self.learning_rate / len(batch_index) * np.dot(self.layers[i - 1].neurons.T, delta)
 			
 	# def test(self, test_input, test_output):
 	# 	result = self.feedforward(test_input)
@@ -178,14 +304,17 @@ test_input = data_test.drop(columns=[0, 1])
 
 from sklearn.neural_network import MLPClassifier
 
-# clf = MLPClassifier(random_state=1, max_iter=300, learning_rate_init=0.1, activation='logistic', batch_size=1, solver='sgd').fit(train_input, train_output)
-# plt.plot(clf.loss_curve_)
-# plt.show()
+# fig, axs = plt.subplots(1, 2)
+clf = MLPClassifier(hidden_layer_sizes=(100, 100, 100, 100), max_iter=40, learning_rate_init=0.001, activation='relu', batch_size=10, solver='sgd', early_stopping=True).fit(train_input, train_output)
+axs[0].plot(clf.loss_curve_, label='scikit')
 
-mlp = MultiLayerPerceptron()
-mlp.add_layer(100, 'sigmoid')
-mlp.add_layer(100, 'sigmoid')
-mlp.add_layer(100, 'sigmoid')
+mlp = MultiLayerPerceptron(optimizer='sgd', epochs=40, batch_size=10, learning_rate=0.001, regul = 0.0001)
+mlp.add_layer(100, 'relu')
+mlp.add_layer(100, 'relu')
+mlp.add_layer(100, 'relu')
+mlp.add_layer(100, 'relu')
+# mlp.add_layer(100, 'relu')
+# mlp.add_layer(100, 'sigmoid')
 # mlp.add_layer(100, 'sigmoid')
 
 
