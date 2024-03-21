7de89c327628f47207ea95a94369021fbcd1fe57
ppajot <ppajot@student.42.fr>
2024-03-21 01:31:36 +0100

    seperrate strat

--- seperate.py
+++ seperate.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
 import pandas as pd
-import math
 import argparse
+
+def	split_df(df: pd.DataFrame, split):
+	train_df = df.sample(None, split)
+	test_df = df.drop(train_df.index)
+	return train_df, test_df
+
 
 parser = argparse.ArgumentParser('Normalizes and split data into train and test files')
 parser.add_argument('-s', '--split', type=float, default=0.8)
@@ -15,21 +20,20 @@ seed = args.random_seed
 
 file = pd.read_csv('data.csv', header = None)
 
-size = file[0].size
-train_size = math.floor(size * split)
-test_size = size - train_size
-
-file = file.sample(None, 1, random_state=seed)
-
 file[file == 'B'] = 0
 file[file == 'M'] = 1
-for i in range (2, 32):
-	file[i] = (file[i] - file[i].min()) / (file[i].max() - file[i].min())
+
+for i in file.columns[2:]:
 	file[i] = (file[i] - file[i].mean()) / file[i].std()
 
+negatives = file[file[1] == 0]
+positives = file[file[1] == 1]
 
-data_train = file.head(train_size)
-data_test = file.tail(test_size)
+negatives_train, negatives_test = split_df(negatives, split)
+positives_train, positives_test = split_df(positives, split)
+
+data_train = pd.concat([negatives_train, positives_train])
+data_test = pd.concat([negatives_test, positives_test])
 
 data_train.to_csv('data_train.csv', header = False, index = False)
 data_test.to_csv('data_test.csv', header = False, index = False)
